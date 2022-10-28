6b44894b3f1aa12fc9159ab41366964d437531fd
Yi Hein Chai <chaiyihein@gmail.com>
2022-10-28 21:18:20 +0100

    Add extension metadata

---
+++ scripts/content.js
@@ -0,0 +1,21 @@
+document.body.onkeyup = function (e) {
+  if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+    if (study.state === "answerShown") {
+      study.answerCard(3);
+    } else {
+      study.drawAnswer();
+    }
+  } else if (e.keyCode == 49 || e.keyCode == 90) {
+    if (study.state === "answerShown") {
+      study.answerCard(1);
+    }
+  } else if (e.keyCode == 50 || e.keyCode == 88) {
+    if (study.state === "answerShown") {
+      study.answerCard(2);
+    }
+  } else if (e.keyCode == 51 || e.keyCode == 86) {
+    if (study.state === "answerShown") {
+      study.answerCard(3);
+    }
+  }
+};
