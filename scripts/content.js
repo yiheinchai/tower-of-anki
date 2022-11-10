e8ebbeeabe781d32c7dce8a64e4758270b800f6d
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2022-11-10 20:29:42 +0000

    better error handling

--- scripts/content.js
+++ scripts/content.js
@@ -1,32 +1,33 @@
-const studyObj = study
-
 function activateFeatures() {
   console.log('attemping to activate...')
   try {
     document.querySelector('#io-overlay').firstChild.setAttribute('style', 'zoom: 1 ;opacity: 1 !important');
   } catch {}
 }
+try {
+  document.body.onkeyup = function (e) {
+    if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+      if (study.state === "answerShown") {
+        study.answerCard(3);
+      } else {
+        study.drawAnswer();
+      }
+    } else if (e.keyCode == 49 || e.keyCode == 90) {
+      if (study.state === "answerShown") {
+        study.answerCard(1);
+      }
+    } else if (e.keyCode == 50 || e.keyCode == 88) {
+      if (study.state === "answerShown") {
+        study.answerCard(2);
+      }
+    } else if (e.keyCode == 51 || e.keyCode == 86) {
+      if (study.state === "answerShown") {
+        study.answerCard(3);
+      }
+    }
+  };
+} catch {
 
-document.body.onkeyup = function (e) {
-  if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-    if (studyObj.state === "answerShown") {
-      studyObj.answerCard(3);
-    } else {
-      studyObj.drawAnswer();
-    }
-  } else if (e.keyCode == 49 || e.keyCode == 90) {
-    if (studyObj.state === "answerShown") {
-      studyObj.answerCard(1);
-    }
-  } else if (e.keyCode == 50 || e.keyCode == 88) {
-    if (studyObj.state === "answerShown") {
-      studyObj.answerCard(2);
-    }
-  } else if (e.keyCode == 51 || e.keyCode == 86) {
-    if (studyObj.state === "answerShown") {
-      studyObj.answerCard(3);
-    }
-  }
-};
+}
 
-setInterval(activateFeatures, 50)
+setInterval(activateFeatures, 10)
