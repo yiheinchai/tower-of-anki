03a364b344ab28e398312c3984c9a5c3f46f04a0
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2022-11-10 19:58:03 +0000

    prevent answer from showing in dark mode, add mutation observer support

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,33 @@
-document.body.onkeyup = function (e) {
-  if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-    if (study.state === "answerShown") {
-      study.answerCard(3);
-    } else {
-      study.drawAnswer();
+
+function activateFeatures() {
+  document.body.onkeyup = function (e) {
+    if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+      if (study.state === "answerShown") {
+        study.answerCard(3);
+      } else {
+        study.drawAnswer();
+      }
+    } else if (e.keyCode == 49 || e.keyCode == 90) {
+      if (study.state === "answerShown") {
+        study.answerCard(1);
+      }
+    } else if (e.keyCode == 50 || e.keyCode == 88) {
+      if (study.state === "answerShown") {
+        study.answerCard(2);
+      }
+    } else if (e.keyCode == 51 || e.keyCode == 86) {
+      if (study.state === "answerShown") {
+        study.answerCard(3);
+      }
     }
-  } else if (e.keyCode == 49 || e.keyCode == 90) {
-    if (study.state === "answerShown") {
-      study.answerCard(1);
-    }
-  } else if (e.keyCode == 50 || e.keyCode == 88) {
-    if (study.state === "answerShown") {
-      study.answerCard(2);
-    }
-  } else if (e.keyCode == 51 || e.keyCode == 86) {
-    if (study.state === "answerShown") {
-      study.answerCard(3);
-    }
-  }
-};
+  };
+  
+  document.querySelector('#io-overlay').firstChild.style.opacity = '1 !important'
+
+}
+const observer = new MutationObserver(activateFeatures);
+observer.observe(document, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
