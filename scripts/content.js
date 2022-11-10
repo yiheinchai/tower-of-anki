0fe90f5550158c85e9a67b941b5b97d3b6f0b868
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2022-11-10 20:57:14 +0000

    fix ele.style bug

--- scripts/content.js
+++ scripts/content.js
@@ -50,5 +50,5 @@ try {
       style.appendChild(document.createTextNode(css));
   }
   head.appendChild(style);
-  document.querySelectorAll('img').forEach((ele) => ele.setAttribute('style', ele.style + ' -webkit-filter: invert(100%); -moz-filter: invert(100%); -o-filter: invert(100%); -ms-filter: invert(100%); '))
+  document.querySelectorAll('img').forEach((ele) => ele.setAttribute('style', ' -webkit-filter: invert(100%); -moz-filter: invert(100%); -o-filter: invert(100%); -ms-filter: invert(100%); '))
 } catch {}
