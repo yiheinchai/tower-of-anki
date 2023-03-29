19706cb2afcdddf47c2434a9d887c4f666167a01
Yi Hein Chai <chaiyihein@gmail.com>
2023-03-29 16:33:11 +0100

    Remove keyboard shortcuts feature as anki added it, added gamification with levels

--- scripts/content.js
+++ scripts/content.js
@@ -1,55 +1,139 @@
-function activateFeatures() {
-  console.log('attemping to activate...')
+function formatDate(date) {
+  let dd = String(date.getDate()).padStart(2, "0");
+  let mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = date.getFullYear();
+  return dd + "/" + mm + "/" + yyyy;
+}
+
+function generateArrayofXElements(x) {
+  return Array.from(
+    {
+      length: x,
+    },
+    (_, i) => i + 1
+  );
+}
+
+function LevelComponent(level, currentReps, totalReps) {
+  return `
+<div style="height: 3rem;" class="level_wrapper">
+ <div>Lvl ${level}</div>
+ <div style="display: grid;grid-template-columns: repeat(${totalReps}, 1fr); grid-template-rows: 1fr;height: 1rem;">
+    ${generateArrayofXElements(totalReps).reduce((acc, ele, index) => {
+      if (index <= currentReps) {
+        return (
+          acc +
+          `<div style="border: 1px solid black; background-color: green;"></div>`
+        );
+      } else {
+        return acc + `<div style = "border: 1px solid black;"></div>`;
+      }
+    }, ``)}</div></div>
+`;
+}
+
+// A function that calculates the level, current xp and next xp given total xp
+// A function that takes an integer of experience and returns the level and the experience to the next level
+function calculateLevel(exp) {
+  // Assume that the level formula is: level = Math.floor(Math.sqrt(exp))
+  // And the experience to the next level formula is: nextExp = (level + 1) * (level + 1) - exp
+  let level = Math.floor(Math.sqrt(exp)); // Calculate the level from the experience
+  let cumulativeExpAtNextLevel = (level + 1) * (level + 1); // Calculate the total experience required for the next level
+  let cumulativeExpAtCurrentLevel = level * level; // Calculate the total experience required for the current level
+  let playerExpAtCurrentLevel = exp - cumulativeExpAtCurrentLevel; // Calculate the experience required for the current level
+  let expToNextLevel = cumulativeExpAtNextLevel - cumulativeExpAtCurrentLevel; // Calculate the experience required for the next level
+  return [level, playerExpAtCurrentLevel, expToNextLevel]; // Return an array with the level and the experience to the next level
+}
+
+function updateLevelDisplay() {
+  level_record = JSON.parse(window.localStorage.getItem("level_record"));
+  const todayDate = formatDate(new Date());
+  if (level_record === null || level_record[todayDate] === undefined) {
+    [level, currentReps, totalReps] = calculateLevel(0);
+    document.querySelector(".pt-1").innerHTML = LevelComponent(
+      level,
+      currentRep,
+      totalReps
+    );
+  } else {
+    const totalXp = level_record[todayDate];
+    [level, currentRep, totalReps] = calculateLevel(totalXp);
+    document.querySelector(".pt-1").innerHTML = LevelComponent(
+      level,
+      currentRep,
+      totalReps
+    );
+  }
+}
+
+function onAnswerUpdateRecord() {
+  level_record = JSON.parse(window.localStorage.getItem("level_record"));
+  const todayDate = formatDate(new Date());
+  if (level_record === null || level_record[todayDate] === undefined) {
+    window.localStorage.setItem(
+      "level_record",
+      JSON.stringify({ [todayDate]: 0 })
+    );
+  } else {
+    currentCount = level_record[todayDate];
+    level_record[todayDate] = currentCount + 1;
+    window.localStorage.setItem("level_record", JSON.stringify(level_record));
+    updateLevelDisplay();
+  }
+}
+
+function activateDarkMode() {
+  console.log("attemping to activate...");
   try {
-    document.querySelector('#io-overlay').firstChild.setAttribute('style', 'zoom: 1 ;opacity: 1 !important');
+    document
+      .querySelector("#io-overlay")
+      .firstChild.setAttribute("style", "zoom: 1 ;opacity: 1 !important");
   } catch {}
-  document.querySelectorAll('img').forEach((ele) => ele.setAttribute('style', ' -webkit-filter: invert(100%); -moz-filter: invert(100%); -o-filter: invert(100%); -ms-filter: invert(100%); '))
+  document
+    .querySelectorAll("img")
+    .forEach((ele) =>
+      ele.setAttribute(
+        "style",
+        " -webkit-filter: invert(100%); -moz-filter: invert(100%); -o-filter: invert(100%); -ms-filter: invert(100%); "
+      )
+    );
 }
 try {
   document.body.onkeyup = function (e) {
-    if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-      if (study.state === "answerShown") {
-        study.answerCard(3);
-      } else {
-        study.drawAnswer();
-      }
-    } else if (e.keyCode == 49 || e.keyCode == 90) {
-      if (study.state === "answerShown") {
-        study.answerCard(1);
-      }
-    } else if (e.keyCode == 50 || e.keyCode == 88) {
-      if (study.state === "answerShown") {
-        study.answerCard(2);
-      }
-    } else if (e.keyCode == 51 || e.keyCode == 86) {
-      if (study.state === "answerShown") {
-        study.answerCard(3);
-      }
+    if (
+      Array.from(document.querySelector("#easebuts").classList).includes(
+        "invisible"
+      )
+    ) {
+      onAnswerUpdateRecord();
     }
   };
-  document.querySelector('#qa').querySelectorAll('span').forEach((e => {e.textContent = e.textContent.toLowerCase(); e.textContent = e.textContent.charAt(0).toUpperCase() + e.textContent.slice(1) }))
-} catch {
-}
+} catch {}
 
-setInterval(activateFeatures, 10)
+setInterval(activateDarkMode, 10);
 
 try {
-  const css = 'html {-webkit-filter: invert(100%);' + '-moz-filter: invert(100%);' + '-o-filter: invert(100%);' + '-ms-filter: invert(100%); }';
-  const head = document.getElementsByTagName('head')[0];
-  const style = document.createElement('style');
-  if (!window.counter) { 
-      window.counter = 1;
-  } else { 
-      window.counter++;
-      if (window.counter % 2 == 0) { 
-          const css = 'html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'
-      } 
-  }
-  style.type = 'text/css';
-  if (style.styleSheet) {
-      style.styleSheet.cssText = css;
+  const css =
+    "html {-webkit-filter: invert(100%);" +
+    "-moz-filter: invert(100%);" +
+    "-o-filter: invert(100%);" +
+    "-ms-filter: invert(100%); }";
+  const head = document.getElementsByTagName("head")[0];
+  const style = document.createElement("style");
+  if (!window.counter) {
+    window.counter = 1;
   } else {
-      style.appendChild(document.createTextNode(css));
+    window.counter++;
+    if (window.counter % 2 == 0) {
+      const css =
+        "html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }";
+    }
+  }
+  style.type = "text/css";
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
   }
   head.appendChild(style);
 } catch {}
